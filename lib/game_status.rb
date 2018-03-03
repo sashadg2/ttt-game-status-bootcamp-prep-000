d965129b0585865654c953623e3fb95ab38ee6ad
Oleksandr Diuriagin <sashadg@gmail.com>
2018-03-03 23:49:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,9 +56,9 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board)[0] == "X"
+  if board[won?(board)[0]] == "X"
     return "X"
-  elsif won?(board)[0] == "O"
+  elsif board[won?(board)[0]] == "O"
     return "O"
   else return nil
   end
